22ea9a9c2c1ef378501a861770d37dc0c865f6b4
AndyGaskell <ag@ssofb.co.uk>
2017-06-06 22:57:57 +0100

    ok

--- src/bigimagefixer.php
+++ src/bigimagefixer.php
@@ -26,7 +26,7 @@ class PlgContentBigimagefixer extends JPlugin
 	protected $app;
 
 
-        /**
+     /**
 	 * Big image fixer after save content method.
 	 * Content is passed by reference, but after the save, so no changes will be saved.
 	 * Method is called right after the content is saved.
@@ -40,121 +40,84 @@ class PlgContentBigimagefixer extends JPlugin
 	 */
 	public function onContentAfterSave($context, $article)
 	{
-            # get the params
-            $maxwidthpixels = (int)$this->params->get('maxwidthpixels', 1000);
-            $maxheightpixels = (int)$this->params->get('maxheightpixels', 1000);
-            $backupbigimage = (int)$this->params->get('backupbigimage', 1);
-            $fixjpg = (int)$this->params->get('fixjpg', 1);
-            $fixpng = (int)$this->params->get('fixpng', 1);
-            $debug = (int)$this->params->get('debug', 0);
-            
-            if ($debug) {
-                $this->app->enqueueMessage("plugin param, maxwidthpixels: " . $maxwidthpixels );
-                $this->app->enqueueMessage("plugin param, maxheightpixels: " . $maxheightpixels );
-                $this->app->enqueueMessage("plugin param, backupbigimage: " . $backupbigimage );
-                $this->app->enqueueMessage("plugin param, fixjpg: " . $fixjpg );
-                $this->app->enqueueMessage("plugin param, fixpng: " . $fixpng );
-                $this->app->enqueueMessage("function context: " . $context );
-                #$this->app->enqueueMessage("function article: <pre>" . print_r($article, TRUE) . "</pre>" );
-                $this->app->enqueueMessage("function introtext: " . $article->introtext );
-                $this->app->enqueueMessage("function fulltext: " . $article->fulltext );
-                
-            }
-            
-            # read in the text
-            $article_text = $article->introtext . " " . $article->fulltext;
-            
-            
-            # find any images in it, making an array
-            $number_of_images = substr_count($article_text, "<img ");
-            if ($debug) $this->app->enqueueMessage("number_of_images: " . $number_of_images );
-            
-            $images_array = array();
-            
-            $doc = new DOMDocument();
-            @$doc->loadHTML($article_text);
+        # get the params
+        $maxwidthpixels = (int)$this->params->get('maxwidthpixels', 1000);
+        $maxheightpixels = (int)$this->params->get('maxheightpixels', 1000);
+        $backupbigimage = (int)$this->params->get('backupbigimage', 1);
+        $fixjpg = (int)$this->params->get('fixjpg', 1);
+        $fixpng = (int)$this->params->get('fixpng', 1);
+        $debug = (int)$this->params->get('debug', 0);
+        
+        if ($debug) {
+            $this->app->enqueueMessage("plugin param, maxwidthpixels: " . $maxwidthpixels );
+            $this->app->enqueueMessage("plugin param, maxheightpixels: " . $maxheightpixels );
+            $this->app->enqueueMessage("plugin param, backupbigimage: " . $backupbigimage );
+            $this->app->enqueueMessage("plugin param, fixjpg: " . $fixjpg );
+            $this->app->enqueueMessage("plugin param, fixpng: " . $fixpng );
+            $this->app->enqueueMessage("function context: " . $context );
+            #$this->app->enqueueMessage("function introtext: " . $article->introtext );
+            #$this->app->enqueueMessage("function fulltext: " . $article->fulltext );
+        }
+        
+        # read in the text
+        $article_text = $article->introtext . " " . $article->fulltext;
 
-            $tags = $doc->getElementsByTagName('img');
+        # find any images in it, making an array
+        $images_array = array();
+        
+        $doc = new DOMDocument();
+        @$doc->loadHTML($article_text);
 
-            if ($debug) $this->app->enqueueMessage("tags: <pre>" . print_r($tags, TRUE) . "</pre>" );
+        $tags = $doc->getElementsByTagName('img');
 
+        if ($debug) $this->app->enqueueMessage("tags: <pre>" . print_r($tags, TRUE) . "</pre>" );
+
+        foreach ($tags as $tag) {
+            echo $tag->getAttribute('src');
+            $images_array[] = $tag->getAttribute('src');
+        }
+
+        if ($debug) $this->app->enqueueMessage("images: <pre>" . print_r($images_array, TRUE) . "</pre>" );
+        if ($debug) $this->app->enqueueMessage("base: " . $_SERVER['DOCUMENT_ROOT'] );
+        
+        # loop through the images 
+        foreach ($images_array as $i=>$image_fn) {
             
-            foreach ($tags as $tag) {
-                echo $tag->getAttribute('src');
-                $images_array[] = $tag->getAttribute('src');
-            }
-
-            if ($debug) $this->app->enqueueMessage("images: <pre>" . print_r($images_array, TRUE) . "</pre>" );
-
+            $image_filepath = JPATH_ROOT . "/" . $image_fn;
             
+            list($width, $height, $type) = getimagesize($image_filepath);
 
+            if ($debug) $this->app->enqueueMessage("image " . $i . " fn: " . $image_filepath );
+            if ($debug) $this->app->enqueueMessage("image " . $i . " width: " . $width );
+            if ($debug) $this->app->enqueueMessage("image " . $i . " height: " . $height );
+            if ($debug) $this->app->enqueueMessage("image " . $i . " type: " . $type );
             
-            if ($debug) $this->app->enqueueMessage("base: " . $_SERVER['DOCUMENT_ROOT'] );
-            
-            # loop through the images 
-            foreach ($images_array as $i=>$image_fn) {
-                
-                
-                
-                $image_filepath = JPATH_ROOT . "/" . $image_fn;
-                
-                list($width, $height, $type) = getimagesize($image_filepath);
+            # check if it's too big
+            if ($width > $maxwidthpixels OR $height > $maxheightpixels ) {
 
-                
-                if ($debug) $this->app->enqueueMessage("image " . $i . " fn: " . $image_filepath );
-                if ($debug) $this->app->enqueueMessage("image " . $i . " width: " . $width );
-                if ($debug) $this->app->enqueueMessage("image " . $i . " height: " . $height );
-                if ($debug) $this->app->enqueueMessage("image " . $i . " type: " . $type );
-                
-                
-                # check if it's too big
-                if ($width > $maxwidthpixels OR $height > $maxheightpixels ) {
-
-                    # check if we're modifying this file type
-                    if ( ($type == 2 AND $fixjpg) OR ($type == 3 AND $fixpng) ) {
-                        
-                        # take a copy if backupbigimage is set
-                        if ( $backupbigimage ) {
-                            $backup_filename = $image_filepath . "_backup";
-                            if ( ($type) == 2 ) $backup_filename .= ".jpg";
-                            if ( ($type) == 3 ) $backup_filename .= ".png";
-                            copy($image_filepath, $backup_filename);
-                            if ($debug) $this->app->enqueueMessage("backed up image " . $i . " image as: " . $backup_filename );
-                        }
-                        
-                        $JImage = new JImage($image_filepath);
-                        
-                        
-                        $JImage->resize($maxwidthpixels, $maxheightpixels, FALSE, $JImage::SCALE_INSIDE);
-                        
-                        
-                        $JImage->toFile($image_filepath, $type);
-                        if ($debug) $this->app->enqueueMessage("Image " . $i . " resized");
-                        
-
-                    } else {
-                        if ($debug) $this->app->enqueueMessage("Image " . $i . " is type not to be resized" );
-                    }                         
+                # check if we're modifying this file type
+                if ( ($type == 2 AND $fixjpg) OR ($type == 3 AND $fixpng) ) {
+                    
+                    # take a copy if backupbigimage is set
+                    if ( $backupbigimage ) {
+                        $backup_filename = $image_filepath . "_backup";
+                        if ( ($type) == 2 ) $backup_filename .= ".jpg";
+                        if ( ($type) == 3 ) $backup_filename .= ".png";
+                        copy($image_filepath, $backup_filename);
+                        if ($debug) $this->app->enqueueMessage("backed up image " . $i . " image as: " . $backup_filename );
+                    }
+                    
+                    $JImage = new JImage($image_filepath);
+                    $JImage->resize($maxwidthpixels, $maxheightpixels, FALSE, $JImage::SCALE_INSIDE);
+                    $JImage->toFile($image_filepath, $type);
+                    if ($debug) $this->app->enqueueMessage("Image " . $i . " resized");
+                    
                 } else {
-                    if ($debug) $this->app->enqueueMessage("Image " . $i . " is ok, not too big" );
-                }
-                
-
-
-                
-            }            
-            
-
-            
-            
-            
-            
-	
-	
-	
-	}
-
-
-
-
+                    if ($debug) $this->app->enqueueMessage("Image " . $i . " is type not to be resized" );
+                }                         
+            } else {
+                if ($debug) $this->app->enqueueMessage("Image " . $i . " is ok, not too big" );
+            }
+        }            
+    }
 }
